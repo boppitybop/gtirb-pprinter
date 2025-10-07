e924fb882c61b04a8eb7f7fb6fbbd2a76d6337c1
Junghee Lim <junghee@rs-caas-debloating-vm.grammatech.com>
2025-10-07 17:02:30 -0400

    Add unit tests for MIPS

--- tests/mips_syntax_test.py
+++ tests/mips_syntax_test.py
@@ -60,6 +60,131 @@ class Mips32SyntaxTest(PPrinterTest):
         self.assertIn("lw $t9,%got_page(fun)($gp)", asm)
         self.assertIn("addiu $t9,$t9,%got_ofst(fun)", asm)
 
+    def test_pcrel_tprel_hi_lo(self):
+        """
+        Test printing pcrel_hi/pcrel_lo and tprel_hi/tprel/lo attributes
+        """
+        ir, m = create_test_module(
+            file_format=gtirb.Module.FileFormat.ELF,
+            isa=gtirb.Module.ISA.MIPS32,
+            byte_order=gtirb.Module.ByteOrder.Little,
+        )
+        s, bi = add_text_section(m)
+
+        insn_bytes = (
+            b"\x00\x00\x19\x3c"  # lui $t9,%pcrel_hi(fun)
+            b"\x00\x00\x39\x27"  # addiu $t9,$t9,%pcrel_lo(fun)
+            b"\x00\x00\x19\x3c"  # lui $t9,%tprel_hi(fun)
+            b"\x00\x00\x39\x27"  # addiu $t9,$t9,%tprel_lo(fun)
+        )
+        add_code_block(bi, insn_bytes)
+
+        insn_bytes = b"\x00\x00\x00\x00"  # nop
+        block_nop = add_code_block(bi, insn_bytes)
+
+        sym = gtirb.symbol.Symbol("fun", payload=block_nop, module=m)
+        sym_expr1 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.PCREL,
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.HI,
+            ],
+        )
+        sym_expr2 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.PCREL,
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.LO,
+            ],
+        )
+        sym_expr3 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.TPREL,
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.HI,
+            ],
+        )
+        sym_expr4 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.TPREL,
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.LO,
+            ],
+        )
+
+        bi.symbolic_expressions[0] = sym_expr1
+        bi.symbolic_expressions[4] = sym_expr2
+        bi.symbolic_expressions[8] = sym_expr3
+        bi.symbolic_expressions[12] = sym_expr4
+
+        asm = run_asm_pprinter(ir)
+
+        # Verify that the instruction is printed correctly.
+        self.assertIn("lui $t9,%pcrel_hi(fun)", asm)
+        self.assertIn("addiu $t9,$t9,%pcrel_lo(fun)", asm)
+        self.assertIn("lui $t9,%tprel_hi(fun)", asm)
+        self.assertIn("addiu $t9,$t9,%tprel_lo(fun)", asm)
+
+    def test_gotpcrel_tlsgd_tlsldm(self):
+        """
+        Test printing gotpcrel, tlsgd, and tlsldm attributes
+        """
+        ir, m = create_test_module(
+            file_format=gtirb.Module.FileFormat.ELF,
+            isa=gtirb.Module.ISA.MIPS32,
+            byte_order=gtirb.Module.ByteOrder.Little,
+        )
+        s, bi = add_text_section(m)
+
+        insn_bytes = (
+            b"\x00\x00\x99\x8f"  # lw $t9,%gottprel(tls_var)($gp)
+            b"\x00\x00\x84\x27"  # addiu $a0,$gp,%tlsgd(tls_var)
+            b"\x00\x00\x84\x27"  # addiu $a0,$gp,%tlsldm(tls_var)
+        )
+        add_code_block(bi, insn_bytes)
+
+        insn_bytes = b"\x00\x00\x00\x00"  # nop
+        block_nop = add_code_block(bi, insn_bytes)
+
+        sym = gtirb.symbol.Symbol("tls_var", payload=block_nop, module=m)
+        sym_expr1 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.GOT,
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.TPREL,
+            ],
+        )
+        sym_expr2 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.TLSGD,
+            ],
+        )
+        sym_expr3 = gtirb.symbolicexpression.SymAddrConst(
+            0,
+            sym,
+            attributes=[
+                gtirb.symbolicexpression.SymbolicExpression.Attribute.TLSLDM,
+            ],
+        )
+
+        bi.symbolic_expressions[0] = sym_expr1
+        bi.symbolic_expressions[4] = sym_expr2
+        bi.symbolic_expressions[8] = sym_expr3
+
+        asm = run_asm_pprinter(ir)
+
+        # Verify that the instruction is printed correctly.
+        self.assertIn("lw $t9,%gottprel(tls_var)($gp)", asm)
+        self.assertIn("addiu $a0,$gp,%tlsgd(tls_var)", asm)
+        self.assertIn("addiu $a0,$gp,%tlsldm(tls_var)", asm)
+
     def test_unsupported_symexpr_attribute_warning(self):
         """
         Test printing unsupported symbolic expression attributes
@@ -100,3 +225,26 @@ class Mips32SyntaxTest(PPrinterTest):
             "representation:",
             output,
         )
+
+    def test_div_divu(self):
+        """
+        Test printing gotpcrel attributes
+        """
+        ir, m = create_test_module(
+            file_format=gtirb.Module.FileFormat.ELF,
+            isa=gtirb.Module.ISA.MIPS32,
+            byte_order=gtirb.Module.ByteOrder.Little,
+        )
+        s, bi = add_text_section(m)
+
+        insn_bytes = (
+            b"\x1a\x00\xa6\x00"  # div $zero,$a1,$a2
+            b"\x1b\x00\x5e\x00"  # divu $zero,$v0,$fp
+        )
+        add_code_block(bi, insn_bytes)
+
+        asm = run_asm_pprinter(ir)
+
+        # Verify that the instruction is printed correctly.
+        self.assertIn("div $zero,$a1,$a2", asm)
+        self.assertIn("divu $zero,$v0,$fp", asm)
